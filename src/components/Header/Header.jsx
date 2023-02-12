402d58724c94e496aecf55e7185a1952896960aa
vivek <vivekjaiswal43348@gmail.com>
2023-02-12 10:19:50 +0530

    react porfolio general css and header responsive component

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import './Header.css'
+import CTA from './CTA'
+// import ME from './../../assets/portfolio-img_1.png'
+// import ME from './../../assets/portfolio-img_2.png'
+// import ME from './../../assets/portfolio-img_3.png'
+import ME from './../../assets/portfolio-img_6.png'
+import HeaderSocials from './HeaderSocials'
+
+const Header = () => {
+  return (
+    <header>
+      <div className="container header_container">
+        <h5>Hello, I'm</h5>
+        <h1>Vivek Jaiswal</h1>
+        <h5 className="text-light">Web Developer</h5>
+
+        <CTA/>
+
+        <HeaderSocials />
+
+        <div className="me">
+          <img src={ME} alt="me" />
+        </div>
+
+        <a href='#contact' className="scroll_down">Scroll Down</a>
+      </div>
+    </header>
+  )
+}
+
+export default Header
