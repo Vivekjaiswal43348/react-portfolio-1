320179c52627df15df59dd9b730d3f3dde529bc2
vivek <vivekjaiswal43348@gmail.com>
2023-02-14 12:24:22 +0530

    responsive experience component done

---
+++ src/components/Experience/experienceData.js
@@ -0,0 +1,62 @@
+export const experienceLevel = {
+    BASIC: "basic",
+    INTERMEDIATE: "intermediate",
+    EXPERIENCED: "experienced",
+};
+export const frontendData = [
+    {
+        technology: "HTML",
+        expLevel: experienceLevel.EXPERIENCED,
+    },
+    {
+        technology: "CSS",
+        expLevel: experienceLevel.EXPERIENCED,
+    },
+    {
+        technology: "Bootstrap",
+        expLevel: experienceLevel.INTERMEDIATE,
+    },
+    {
+        technology: "SASS",
+        expLevel: experienceLevel.EXPERIENCED,
+    },
+    {
+        technology: "Material UI",
+        expLevel: experienceLevel.BASIC,
+    },
+    {
+        technology: "JAVASCRIPT",
+        expLevel: experienceLevel.EXPERIENCED,
+    },
+    {
+        technology: "ReactJS",
+        expLevel: experienceLevel.EXPERIENCED,
+    },
+    {
+        technology: "Typescript",
+        expLevel: experienceLevel.EXPERIENCED,
+    },
+];
+
+export const backendData = [
+    {
+        technology: "NodeJS",
+        expLevel: experienceLevel.BASIC,
+    },
+    {
+        technology: "Java/OOPs",
+        expLevel: experienceLevel.BASIC,
+    },
+    {
+        technology: "MySQL",
+        expLevel: experienceLevel.BASIC,
+    },
+    {
+        technology: "MogoDB",
+        expLevel: experienceLevel.BASIC,
+    },
+    {
+        technology: "GIT",
+        expLevel: experienceLevel.EXPERIENCED,
+    },
+];
