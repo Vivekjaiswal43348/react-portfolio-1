65300b618db4d10bf4ec9af25c3174308011e003
vivek <vivekjaiswal43348@gmail.com>
2023-02-16 18:51:04 +0530

    dynamic testimonials component created before swipre JS

--- src/components/Testimonials/Testimonials.jsx
+++ src/components/Testimonials/Testimonials.jsx
@@ -1,10 +1,28 @@
-import React from 'react'
-import './Testimonials.css';
+import React from "react";
+import "./Testimonials.css";
+import Avatar1 from "./../../assets/avatar1.jpg";
+import { testiMonialsData } from "./TestimonialsData";
 
 const Testimonials = () => {
   return (
-    <section>Testimonials</section>
-  )
-}
+    <section>
+      <h5>Review from clients</h5>
+      <h2>Testimonials</h2>
+      <div className="container testmonial_container">
+        {testiMonialsData.map(({ avatar, name, review }, index) => {
+          return (
+            <article className="testimonial" key={index}>
+              <div className="client_avatar">
+                <img src={avatar} alt="" />
+              </div>
+              <h4>{name}</h4>
+              <small className="client_review">{review}</small>
+            </article>
+          );
+        })}
+      </div>
+    </section>
+  );
+};
 
-export default Testimonials
+export default Testimonials;
---
+++ src/components/Testimonials/TestimonialsData.js
@@ -0,0 +1,27 @@
+import Avatar1 from "./../../assets/avatar1.jpg";
+import Avatar2 from "./../../assets/avatar2.jpg";
+import Avatar3 from "./../../assets/avatar3.jpg";
+import Avatar4 from "./../../assets/avatar4.jpg";
+
+export const testiMonialsData = [
+    {
+        avatar: Avatar1,
+        name: 'Tina Snow',
+        review: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo fuga omnis voluptatibus amet cupiditate necessitatibus cumque dolorum repellat, blanditiis facere debitis non tenetur sed nulla?'
+    },
+    {
+        avatar: Avatar2,
+        name: 'Shatta Wale',
+        review: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ea animi soluta incidunt laudantium deleniti id voluptate, aut pariatur. Vero delectus autem officiis dignissimos. Dolores dolorum dicta incidunt atque unde provident?'
+    },
+    {
+        avatar: Avatar3,
+        name: 'Kwame Despite',
+        review: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit tempora, debitis nam sunt nulla ex odio aliquid nobis quaerat eaque rem repellat libero reiciendis ea magni nihil exercitationem?'
+    },
+    {
+        avatar: Avatar4,
+        name: 'Nana Ama',
+        review: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ea animi soluta incidunt laudantium deleniti id voluptate, aut pariatur.'
+    }
+]
