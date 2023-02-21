7762325e556ded2f311e75902ef0798858524d83
vivek <vivekjaiswal43348@gmail.com>
2023-02-21 19:48:57 +0530

    ReactDOM.render() no support in react 18

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
-import ReactDOM from 'react-dom'
+// import ReactDOM from 'react-dom'
+import { createRoot } from 'react-dom/client';
 import App from './App'
 import './index.css'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// ReactDOM.createRoot (<App />, document.getElementById('root'));
+const rootElem =document.getElementById('root');
+const root = createRoot(rootElem);
+root.render(<App />);
